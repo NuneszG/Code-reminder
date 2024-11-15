a9fa33195317544b18615e715a8d19736f00e0cb
GuilhermeNunes <digobros13@gmail.com>
2024-11-14 23:24:56 -0300

    frontend, done. Just created func to create and get folders in db

--- reminder/urls.py
+++ reminder/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import (
     Home,
+    Folders,
+    Form, 
+    Create,
 )
 
 urlpatterns = [
-    path('', Home, name='home')
+    path('', Home, name='home'),
+    path('form-folder/', Form, name='form'),
+    path('create-folder/', Create, name='create'),
+    path('folders/', Folders, name='folders'),
+
 ]
--- reminder/views.py
+++ reminder/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+from .models import Folder
 
 def Home(request):
-    return render(request, 'home.html')
+    folders = Folder.objects.all()
+    return render(request, 'home.html', {'folder': folders})
+
+
+def Folders(request):
+    folders = Folder.objects.all()
+    return render(request, './folder/folders.html', {'folders': folders})
+
+
+def Form(request):
+    return render(request, './folder/form.html')
+
+def Create(request):
+    title = request.POST.get('title')
+    category = request.POST.get('category')
+    descripion = request.POST.get('description')
+
+    folder = Folder.objects.create(
+        title = title,
+        category = category,
+        description = descripion
+    )
+    return render(request, 'home.html', {'folder': folder})
